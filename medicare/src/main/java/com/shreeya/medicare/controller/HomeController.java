d840f0e7595ba5b32ca7c0038bdd54ca4817e0fb
Shreeya Kumari Ram <shreeya.kumari.ram2736@gmail.com>
2026-07-11 22:21:16 +0530

    Add GreetingService and implement Dependency Injection

--- medicare/src/main/java/com/shreeya/medicare/controller/HomeController.java
+++ medicare/src/main/java/com/shreeya/medicare/controller/HomeController.java
@@ -1,19 +1,24 @@
 package com.shreeya.medicare.controller;
 
+import com.shreeya.medicare.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    GreetingService greetingService;
+
     @GetMapping("/")
     public String home() {
-        return "Welcome to MediCare Backend";
+        return greetingService.welcomeMessage();
     }
 
     @GetMapping("/about")
     public String about() {
-        return "This is our first Spring Boot Project";
+        return greetingService.aboutMessage();
     }
 
 }
